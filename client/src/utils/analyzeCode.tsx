8369c7e626acfda8b5d8932d47d0ef523426a2d0
Musyonchez <musyonchez@gmail.com>
2024-08-10 02:22:03 +0300

    create nextapp and initial success

---
+++ client/src/utils/analyzeCode.tsx
@@ -0,0 +1,17 @@
+export default async function analyzeCode(code: string, language: string) {
+  const response = await fetch("/api/analyze", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ code, language }),
+  });
+  const text = await response.text();
+  console.log("Raw response:", text); // Log the raw response for debugging
+  // try {
+  //   const data = await response.json();
+  //   console.log(data.ast);
+  // } catch (error) {
+  //   console.error("Failed to parse JSON:", error);
+  // }
+}
