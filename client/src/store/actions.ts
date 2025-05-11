4d9f7103cdfab087346179a2d4b3e9734c9ee17d
Musyonchez <musyonchez@gmail.com>
2025-05-11 18:15:25 +0300

    backup working at may 11 at 6:15pm finished adding loading etc

--- client/src/store/actions.ts
+++ client/src/store/actions.ts
@@ -1,4 +1,6 @@
+// actions.ts
 export const FETCH_SNIPPETS = "FETCH_SNIPPETS";
+export const SET_ADD_SNIPPET_STATUS = "SET_ADD_SNIPPET_STATUS";
 export const SET_SNIPPETS = "SET_SNIPPETS";
 export const ADD_SNIPPET = "ADD_SNIPPET";
 
@@ -10,11 +12,24 @@ export interface SnippetPayload {
   userId: number;
 }
 export const fetchSnippets = () => ({ type: FETCH_SNIPPETS });
+
+
+
 export const setSnippets = (snippets: any[]) => ({
   type: SET_SNIPPETS,
   payload: snippets,
 });
+
+
 export const addSnippet = (payload: SnippetPayload) => ({
   type: ADD_SNIPPET,
   payload,
+});
+
+
+
+
+export const setAddSnippetStatus = (status: "idle" | "loading" | "success" | "error") => ({
+  type: SET_ADD_SNIPPET_STATUS,
+  payload: status,
 });
