72a9fd8d516b360cf9668925ebcb0d11993e1271
Musyonchez <musyonchez@gmail.com>
2025-05-30 04:43:21 +0300

    adding mobile responsiveness

--- client/src/components/snippets/RandomButtons.tsx
+++ client/src/components/snippets/RandomButtons.tsx
@@ -1,31 +1,31 @@
 interface RandomButtonsProps {
-    onOpenRandom: (type: "all" | "favorites" | "frequent") => void;
-  }
-  
-  const RandomButtons: React.FC<RandomButtonsProps> = ({ onOpenRandom }) => {
-    return (
-      <div className="ml-auto flex gap-2">
-        <button
-          onClick={() => onOpenRandom("all")}
-          className="bg-gray-700 text-white px-3 py-1 rounded"
-        >
-          🎲 Random
-        </button>
-        <button
-          onClick={() => onOpenRandom("favorites")}
-          className="bg-yellow-600 text-white px-3 py-1 rounded"
-        >
-          ⭐ Favorites
-        </button>
-        <button
-          onClick={() => onOpenRandom("frequent")}
-          className="bg-purple-600 text-white px-3 py-1 rounded"
-        >
-          🔁 Frequent
-        </button>
-      </div>
-    );
-  };
-  
-  export default RandomButtons;
-  
+  onOpenRandom: (type: "all" | "favorites" | "frequent") => void;
+}
+
+const RandomButtons: React.FC<RandomButtonsProps> = ({ onOpenRandom }) => {
+  return (
+    <div className=" md:ml-auto flex gap-2">
+      <button
+        onClick={() => onOpenRandom("all")}
+        className="bg-gray-700 text-white px-3 py-1 rounded"
+      >
+        🎲 Random
+      </button>
+      <button
+        onClick={() => onOpenRandom("favorites")}
+        className="bg-yellow-600 text-white px-3 py-1 rounded"
+      >
+        ⭐ Favorites
+      </button>
+      <button
+        onClick={() => onOpenRandom("frequent")}
+        className="bg-purple-600 text-white px-3 py-1 rounded"
+      >
+        🔁 Frequent
+      </button>
+    </div>
+  );
+};
+
+export default RandomButtons;
+
